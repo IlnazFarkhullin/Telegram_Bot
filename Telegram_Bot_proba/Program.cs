b04d89de6071896ea8fb7a05de50165f030df168
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-07-21 16:09:07 +0300

    Вывод пришедших сообщений на консоль. Создана функция ответа бота тем же сообщением что и отправлено

--- Telegram_Bot_proba/Program.cs
+++ Telegram_Bot_proba/Program.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace Telegram_Bot_proba
 {
     class Program
     {
         //Мой первый простой ТЕЛЕГРАММ БОТ
-        // Для начала устанавливаем NuGet пакет : Telegram.Bot версия 15.7.1
-        private static string token { get; set; } = "5534709509:AAFdzjbX7Dv3QBPi9GFVuqtK2Pz0vd8jjVY"; //Токен для из бота в котором мы зарегистрировали бота 
+        // Для начала устанавливаем NuGet пакет : Telegram.Bot версия 16.0.0 (использовался именно эта версия пакета из-за того что в последней версии нет того или иного метода)
+        public static string token  = "5423160433:AAFlh4eOZBh3uilipEhvha98S8xHNQCn8Ck"; //Токен для из бота в котором мы зарегистрировали бота 
         private static TelegramBotClient client;//поле для работы с клиентом    
-      
+
+        [Obsolete] // озночает что тот или иной метод устарел
         static void Main(string[] args)
         {
-          client = new TelegramBotClient(token); // инициализируем бота используя наш токен
-          client.StartReceiving(); //начало приёма сообщений
-            client.OnMessage += OnMessageHeadler();// привязка к событию OnMessage и метод где будет происходить все действия с ботомж; 
+            client = new TelegramBotClient(token); // инициализируем бота используя наш токен
+            client.StartReceiving(); //начало приёма сообщений
+            client.OnMessage += OnMessageHeadler;// привязка к событию OnMessage и метод где будет происходить все действия с ботомж; 
           Console.ReadLine();
           client.StopReceiving(); //конец приёма сообщений
            
+        }
+
+        
+        private static async void  OnMessageHeadler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;  //локальная переменная для записи полученного сообщения
+            if (msg != null)// проверка на наличие сообщения
+            {
+                Console.WriteLine($"Пришло сообщение с текстом {msg.Text}");//вывод пришедшего сообщения на консоль
+                await client.SendTextMessageAsync(msg.Chat.Id, msg.Text, replyToMessageId: msg.MessageId);//ответ бота
+            }
         }
     }
 }
